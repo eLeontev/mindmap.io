30f4449b6e1e43048dec818a263aea7e22786e88
eLeontev <joonior199@gmail.com>
2019-06-29 20:29:33 +0300

    set valid repo name for gh pages

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -5,7 +5,7 @@ ghpages.publish(
   'public',
   {
     branch: 'master',
-    repo: 'https://github.com/flexdinesh/flexdinesh.github.io.git',
+    repo: 'https://github.com/eLeontev/gatsby_mind-map.github.io.git',
   },
   () => {
     console.log('Deploy Complete!')
